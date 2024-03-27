ba42676e3b6797bfa2932ddd95bc1fd13faacfe9
Colin <cfinn68916@gmail.com>
2024-03-27 18:40:38 -0500

    Add feedback and write to svg
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,40 +1,91 @@
 #include <iostream>
 #include "cmath"
-#include "vector"
 #include "src/VecUtils.h"
 #include "src/ArmDynamics.h"
+#include "src/ProfileUtils.h"
+#include <fstream>
+#include <sstream>
 
 
-// {theta1,theta2,omega1,omega2}
-State propagate(State state, double dt){
-    Vec2 a=alpha(state.pos,state.vel, torques({1,1},{0,0},{0,0}));
+State propagate(State state, Vec2 u, double dt){
+    Vec2 a=alpha(state.pos,state.vel, u);
     return {state.pos+(state.vel+a*dt*0.5)*dt,state.vel+(a*dt)};
 }
 
-State euler(State state, double totalT, double dt){
+State euler(State state, double totalT, double dt, Vec2 u){
     double t=0;
     State statecp=state;
     while(totalT-t>dt){
-        statecp=propagate(statecp, dt);
+        statecp=propagate(statecp,u, dt);
         t+=dt;
     }
-    return propagate(statecp, totalT-t);
-}
-double sq(double i){
-    return i*i;
+    return propagate(statecp,u, totalT-t);
 }
 
-double vdiff(std::vector<double> state1,std::vector<double> state2){
-    return sq(state1[0]-state2[0])+sq(state1[1]-state2[1])+sq(state1[2]-state2[2])+sq(state1[3]-state2[3]);
+Vec2 torquesForGoal(State s){
+    double goal_a=-M_PI;
+    double goal_b=-M_PI/4;
+    return torques(s.pos,s.vel,{calcAccel(s.vel.a,s.pos.a,goal_a,1,2),calcAccel(s.vel.b,s.pos.b,goal_b,1,2)});
+}
+
+
+
+std::string toTwoHex(int inp){
+    std::stringstream stre{};
+    stre << std::hex << inp;
+    std::string res(stre.str());
+    while(res.size()<2){
+        res.insert(res.cbegin(),'0');
+    }
+    return res;
+}
+std::string toTwoHex(double inp){
+    return toTwoHex((int) inp);
+}
+
+Vec2 angleToSvg(Vec2 theta){
+    Vec2 pos= anglesToPos(theta);
+    return {pos.a*100+300,300-100*pos.b};
 }
 
 
 int main() {
-    State st2{{1.0, 1.0}, {0, 0}};
-    for (int i = 0; i < 1000; i++) {
+
+    std::ofstream f;
+    f.open("out.svg");
+    f << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
+         "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n"
+         "<svg width=\"4000\" height=\"4000\" viewBox=\"-70.5 -70.5 4000 4000\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
+
+
+    f << "<line x1=\"" << 280 << "\" y1=\"" << 300 << "\" x2=\"" << 320<< "\" y2=\"" << 300<< "\" stroke=\"#0000FF\" stroke-width=\"1\" />";
+
+
+
+    State st2{{2.0, 2.0}, {0, 0}};
+
+    f << "<line x1=\"" << 300 << "\" y1=\"" << 300 << "\" x2=\"" << 300+100*ArmConstants::l1*cos(st2.pos.a)<< "\" y2=\"" << 300-100*ArmConstants::l2*sin(st2.pos.a)<< "\" stroke=\"#FF00FF\" stroke-width=\"1\" />";
+    f << "<line x1=\"" << 300+100*ArmConstants::l1*cos(st2.pos.a) << "\" y1=\"" << 300-100*ArmConstants::l2*sin(st2.pos.a) << "\" x2=\"" << 300+100*(ArmConstants::l1*cos(st2.pos.a)+ArmConstants::l2*cos(st2.pos.a+st2.pos.b)) << "\" y2=\"" << 300-100*(ArmConstants::l1*sin(st2.pos.a)+ArmConstants::l2*sin(st2.pos.a+st2.pos.b)) << "\" stroke=\"#FF00FF\" stroke-width=\"1\" />";
+
+
+    State st3{{2.0, 2.0}, {0, 0}};
+    for (int i = 0; i < 300; i++) {
 //        std::cout << 0.02 * i  << "     " << st2.pos.a << " " << st2.pos.b << "\n";
-        st2 = euler(st2, 0.02, 3E-7);
+        st2 = euler(st2, 0.02, 1E-6, torquesForGoal(st2));
+
+        f << "<line x1=\"" << angleToSvg(st3.pos).a << "\" y1=\"" << angleToSvg(st3.pos).b << "\" x2=\"" << angleToSvg(st2.pos).a<< "\" y2=\"" << angleToSvg(st2.pos).b << "\" stroke=\"#"
+          << toTwoHex(256.0*i/300.0) <<toTwoHex((256.0*(300.0-i)/300.0)) << "00\" stroke-width=\"1\" />";
+
+        st3=st2;
     }
+
+    f << "<line x1=\"" << 300 << "\" y1=\"" << 300 << "\" x2=\"" << 300+100*ArmConstants::l1*cos(st2.pos.a)<< "\" y2=\"" << 300-100*ArmConstants::l2*sin(st2.pos.a)<< "\" stroke=\"#00FFFF\" stroke-width=\"1\" />";
+    f << "<line x1=\"" << 300+100*ArmConstants::l1*cos(st2.pos.a) << "\" y1=\"" << 300-100*ArmConstants::l2*sin(st2.pos.a) << "\" x2=\"" << 300+100*(ArmConstants::l1*cos(st2.pos.a)+ArmConstants::l2*cos(st2.pos.a+st2.pos.b)) << "\" y2=\"" << 300-100*(ArmConstants::l1*sin(st2.pos.a)+ArmConstants::l2*sin(st2.pos.a+st2.pos.b)) << "\" stroke=\"#00FFFF\" stroke-width=\"1\" />";
+
+
+    f << "</svg>";
+    f.close();
+
 
 
     return 0;
--- src/ProfileUtils.h
+++ src/ProfileUtils.h
@@ -1,3 +1,3 @@
 #pragma once
 
-static double calcAccel(double vel, double pos, double goal, double maxVel, double maxAccel);
+double calcAccel(double vel, double pos, double goal, double maxVel, double maxAccel);
