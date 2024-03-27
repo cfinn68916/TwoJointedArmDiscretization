68008c7c7a10ab3b604d6a17be45ad1df7e30ad1
Colin <cfinn68916@gmail.com>
2024-03-27 17:10:40 -0500

    Add 6328 constants
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/ArmDynamics.h
+++ src/ArmDynamics.h
@@ -3,16 +3,16 @@
 #include "VecUtils.h"
 
 namespace ArmConstants{
-    const double I1 = 1.0;
-    const double I2 = 1.0;
-    const double m1 = 1.0;
-    const double m2 = 1.0;
-    const double r1 = 0.5;
-    const double r2 = 0.5;
-    const double l1 = 1.0;
-    const double l2 = 1.0;
+    const double I1 = 0.493;
+    const double I2 = 0.362;
+    const double m1 = 4.16;
+    const double m2 = 1;
+    const double r1 = 0.269;
+    const double r2 = 0.243;
+    const double l1 = 0.638429;
+    const double l2 = 0.80645;
 }
-const double g = 9.8;
+const double g = 9.8065;
 
 Vec2 tg(Vec2 theta);
 Mat coriolis(Vec2 theta, Vec2 omega);
